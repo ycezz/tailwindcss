31f365b1b6321c5c04eab4463bacb44d78e497fc
Putri Mila <putrimiila.fx@gmail.com>
2024-08-27 15:53:43 +0800

    add all

--- 02.basic-utility/utility-bagian1/tailwind.config.js
+++ 02.basic-utility/utility-bagian1/tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     extend: {
       spacing: {
         '13': '3.25rem',
-      }
+      },
+      fontFamily: {
+        inter: ['Inter'],
+      },
+      colors: {
+        putri: '#bada55',
+        kopi: '#c0ffee'
+      },
     },
   },
   plugins: [],
