b0a3fc0c71aaf4784fe224d886b8729a53b17a7a
Putri Mila <putrimiila.fx@gmail.com>
2024-10-09 23:07:52 +0800

    add

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      general: ["General Sans", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        brand: {
+          primary: "#3461FF",
+          secondary: "#000000",
+          gray: "#FAFAFA",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
