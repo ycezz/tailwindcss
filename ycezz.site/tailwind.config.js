48263b81d4012a43d7e7e4b15778ba766f8b59d2
Putri Mila <putrimiila.fx@gmail.com>
2024-10-16 23:56:26 +0800

    add

---
+++ ycezz.site/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      space: ["Space Grotesk", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
